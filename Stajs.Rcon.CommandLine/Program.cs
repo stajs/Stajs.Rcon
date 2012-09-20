ec9644e3cf3e06a6efe9e504fd31f1d007c19d7f
Shannon Skinner <shannonsnz@gmail.com>
2012-09-20 21:30:09 +1200

    Initial commit

---
+++ Stajs.Rcon.CommandLine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stajs.Rcon.CommandLine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string ipAddress = "0.0.0.0";
+			const int port = 27015;
+			const string password = "blah";
+
+			var rcon = new Core.Rcon(ipAddress, port, password);
+			rcon.Connect();
+		}
+	}
+}
---
+++ Stajs.Rcon.Core/Rcon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Stajs.Rcon.Core
+{
+	public class Rcon
+	{
+		private readonly IPAddress _ipAddress;
+		private readonly int _port;
+
+		public Rcon(string ipAddress, int port, string password) : this(IPAddress.Parse(ipAddress), port, password)
+		{
+			
+		}
+
+		public Rcon(IPAddress ipAddress, int port, string password)
+		{
+			_ipAddress = ipAddress;
+			_port = port;
+		}
+
+		public void Connect()
+		{
+			var server = new IPEndPoint(_ipAddress, _port);
+			var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			socket.Connect(server);
+			
+			socket.Disconnect(true);
+		}
+	}
+}
